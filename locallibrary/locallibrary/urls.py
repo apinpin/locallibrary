38a0d38f597b7682f46d0f3879e66e5ae95d1ae2
apinpin <andropinpin@gmail.com>
2018-03-14 23:54:39 -0700

    changes

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
