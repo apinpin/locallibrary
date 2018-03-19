0f0c654198a5d9cd58b906cfcda02ce255192b8c
apinpin <andropinpin@gmail.com>
2018-03-18 18:24:42 -0700

    improved upon basic admin model views.  Added extra details with inlines

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -7,17 +7,38 @@ admin.site.register(Genre)
 #admin.site.register(Book)
 #admin.site.register(BookInstance)
 
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
 # Define the Admin class
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 # Register Admin class with associated model
 admin.site.register(Author, AuthorAdmin)
 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
-@admin.register(BookInstance):
+@admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('book', 'id', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
