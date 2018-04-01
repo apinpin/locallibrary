a42eb6707b4bd681f21aaa170d24ce1a49c07378
apinpin <andropinpin@gmail.com>
2018-03-31 22:38:19 -0700

    Added test for model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -96,7 +96,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
 
     class Meta:
         ordering = ["last_name", "first_name"]
@@ -111,6 +111,6 @@ class Author(models.Model):
         """
         String for representing the Model object
         """
-        return '{0},{1}'.format(self.last_name, self.first_name)
+        return '{0}, {1}'.format(self.last_name, self.first_name)
 
 
